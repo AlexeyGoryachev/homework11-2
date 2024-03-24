9713530628997c454c36c358135ff9e2ac53f9ab
AlexeyGoryachev <iwmoney11977@gmail.com>
2024-03-25 02:17:26 +0300

    complete task 3.

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,14 @@ public class Main {
         int os = 0;
         int clientDeviceYear = 2024;
         selectingVersion(os, clientDeviceYear);
+        System.out.println("Task 3.");
+        int deliveryDistance = 99;
+        int deliveryTime = calculateDeliveryTime(deliveryDistance);
+        if (deliveryTime == -1) {
+            System.out.println("Доставка не осуществляется");
+        } else {
+            System.out.println("На доставку потребуется " + deliveryTime + " день/дня.");
+        }
     }
     public static void checkYear(int year) {
         if ((year % 100 != 0 && year % 4 == 0) || (year % 400 == 0)) {
@@ -31,5 +39,16 @@ public class Main {
         } else if (os == 1 && clientDeviceYear > 2015) {
             System.out.println("установите версию приложения для Android по ссылке");
         }
+    }
+    public static int calculateDeliveryTime(int deliveryDistance) {
+        int deliveryTime = 1;
+        if (deliveryDistance > 20 && deliveryDistance <= 60) {
+            deliveryTime = deliveryTime * 2;
+        } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
+            deliveryTime = deliveryTime * 3;
+        } else if (deliveryDistance > 100) {
+            deliveryTime = -1;
+        }
+        return deliveryTime;
     }
 }
