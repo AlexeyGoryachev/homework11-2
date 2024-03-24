44bcbc3e598329c565d257f83fe4b0d7c72e9eb7
AlexeyGoryachev <iwmoney11977@gmail.com>
2024-03-25 02:13:18 +0300

    complete task 1.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Homework 11.");
+        System.out.println("Task 1.");
+        int thisYear = 2024;
+        checkYear(thisYear);
+    }
+    public static void checkYear(int year) {
+        if ((year % 100 != 0 && year % 4 == 0) || (year % 400 == 0)) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
     }
 }
